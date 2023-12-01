a0ccc12c367491e00e770057e3a62bbe6ee5943b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-12-01 18:38:46 +0000

    fix pipeline.ts

--- src/dagger/pipeline.ts
+++ src/dagger/pipeline.ts
@@ -12,7 +12,11 @@ export default async function pipeline(src = ".", args: string[] = []) {
     return;
   }
 
-  await migrate();
+  await migrate(
+    src,
+    Deno.env.get("DATABASE_URL")!,
+    Deno.env.get("DATABASE_DEV_URL")
+  );
 }
 
 async function runSpecificJobs(args: jobs.Job[]) {
@@ -21,6 +25,10 @@ async function runSpecificJobs(args: jobs.Job[]) {
     if (!job) {
       throw new Error(`Job ${name} not found`);
     }
-    await job();
+    await job(
+      ".",
+      Deno.env.get("DATABASE_URL")!,
+      Deno.env.get("DATABASE_DEV_URL")
+    );
   }
 }
